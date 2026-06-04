a0653c7d2f957d5cd1bb36331b8eeddfdb460c5e
Ayush Shekhar Singh <ayush@Ayushs-MacBook-Air.local>
2026-06-04 14:48:05 +0530

    Add DAY--8 folder

---
+++ DAY--8/Ques-31.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, j;
+
+    cout<<"Enter number of rows u want to print: ";
+    cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        char ch='A';
+
+        for(j=1;j<=i;j++)
+        {
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+
+}
