934af2db5db405c295595e7fdd7b5f0cba64e8f5
Ayush Shekhar Singh <ayush@Ayushs-MacBook-Air.local>
2026-06-05 22:59:11 +0530

    Add DAY--9 folder

---
+++ DAY--9/ques-34.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, j;
+
+    cout<<"Enter number of rows u want to print: ";
+    cin>>n;
+
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<j<< " ";
+        }
+        cout<<endl;
+    }
+
+}
